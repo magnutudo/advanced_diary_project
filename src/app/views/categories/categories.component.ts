b5f33714a18595ba5fbbc15b2a92acb910d75f8a
Эмиль <vagab@>
2023-06-08 23:12:52 +0300

    added a data server

--- src/app/views/categories/categories.component.ts
+++ src/app/views/categories/categories.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DataHandlerService} from "../../service/data-handler.service";
+import {Category} from "../../model/Category";
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
   styleUrls: ['./categories.component.css']
 })
-export class CategoriesComponent {
+export class CategoriesComponent implements OnInit {
+  categories:Category[]
+  constructor(private dataService: DataHandlerService) {
+  }
+
+  ngOnInit(): void {
+    this.categories = this.dataService.getCategories()
+  }
 
 }
